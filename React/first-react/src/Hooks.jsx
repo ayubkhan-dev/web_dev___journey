49815aba28a3615034c958e4a53e940c341c8cbd
ayubkhan-dev <ayubkhan03295642@gmail.com>
2026-09-13 01:26:10 +0500

    Hook in React practise

---
+++ React/first-react/src/Hooks.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react"
+import './App.css'
+
+
+function Hooks(){
+
+    let [counter,setCounter] = useState(1);
+    // let counter = 15
+
+    const addvalue = ()=>{
+        if(counter >0 && counter <20){
+        counter = counter +1
+        setCounter(counter)
+        }
+        
+    }
+
+    const removevalue = () => {
+        if(counter <=20 && counter >1){
+        counter = counter-1
+        setCounter(counter)
+
+        }
+       
+    }
+
+
+
+    return (
+        <>
+        <h1>chia aur react ka course</h1>
+        <h2>counter value : {counter}</h2>
+        <button onClick={addvalue}>Add value {counter}</button>
+        <br />
+        <button onClick={removevalue}>Remove value {counter}</button>
+        <p>Footer : {counter} </p>
+        </>
+    )
+}
+export default Hooks
+
+
+
+
+
+
+
+                                           // practice in hook
+// function Hooks() {
+
+//    let [counter,setCounter] = useState(20)
+
+//     // let counter = 20
+//     const addvalue = () =>{
+         
+//         counter= counter+1
+//         setCounter(counter)
+//         console.log("clicked" , counter) 
+//     }
+
+//     const removevalue =()=>{
+//         counter = counter - 1
+//         setCounter (counter)
+//     }
+  
+//     return(
+//         <>
+//         <h1>chia our react</h1>
+//         <h3>counter value: {counter}</h3>
+//         <button onClick={addvalue}> Add value {counter} </button>
+//         <br />
+//         <button onClick={removevalue}>Remove value {counter}</button>
+//         <p>Footer : {counter}</p>
+//         </>
+//    )
+// }
+// export default Hooks;
--- React/first-react/src/main.jsx
+++ React/first-react/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Ayub from './Ayub.jsx'
+import App from './App.jsx';
+import Ayub from './Ayub.jsx';
+import Hooks from './Hooks.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Ayub/>
+    <Hooks/>
   </StrictMode>,
 )
